ffe313a3f2645c0fe37600188bcb72f67b3242b4
Engineer (via Ion Vrinceanu) <ion@vrinceanu.ro>
2026-02-07 17:21:24 +0000

    fix: remove duplicate Config.swift in Watch App

--- iOS/watchOS/VRN Workouts/VRN Workouts Watch App/Config.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-enum Config {
-    static let supabaseURL = "https://fpatywrdrltaeftjjyjj.supabase.co"
-    static let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwYXR5d3Jkcmx0YWVmdGpqeWpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5MTEzODQsImV4cCI6MjA4NTQ4NzM4NH0.ZrKpJ0Yrti3yUxmfzsN7XjJupf1zAeG_Ad9Iq7eJn-o"
-}
